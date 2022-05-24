ce7507d5095839a616df30c80b996a8c37177ac5
astapovich <46627721+astapovich@users.noreply.github.com>
2022-05-24 23:57:59 +0300

    Update build.js

--- build.js
+++ build.js
@@ -16,11 +16,11 @@ StyleDictionaryPackage.registerTransform({
     type: 'value',
     matcher: function(prop) {
         // You can be more specific here if you only want 'em' units for font sizes    
-         return prop.attributes.category === 'fontSize';
+        return ["fontSize", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
     },
     transformer: function(prop) {
         // You can also modify the value here if you want to convert pixels to ems
-       return parseFloat(prop.original.value) + 'px';
+        return parseFloat(prop.original.value) + 'px';
     }
     });
 
